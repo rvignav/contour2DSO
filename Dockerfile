c38a8462d8fab24a67d967d57cfe7dbdf5f14989
rvignav <rvignav@gmail.com>
2020-09-03 13:55:32 -0700

    Create seg_mask array

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3
 ADD createDSO.py /
 RUN apt-get update -y
 RUN apt-get install -y libgl1-mesa-glx
-RUN pip install opencv-python pydicom numpy argparse Pillow glob2 progressbar
+RUN pip install opencv-python pydicom numpy argparse Pillow glob2
 ENTRYPOINT [ "python", "./createDSO.py" ]
--- createDSO.py
+++ createDSO.py
@@ -4,7 +4,9 @@ import argparse
 from PIL import Image  
 import glob
 import os
-import progressbar
+import json
+from matplotlib.path import Path
+from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser(description='Create DSO')
 parser.add_argument('series_path', type=str, help='Path to series')
@@ -37,28 +39,65 @@ def bubble_sort(series):
                 swapped = True
     return series
 
-seriesDCM = glob.glob(str(abs_path) + "/*.dcm")
+def visualize(seg_mask):
+  vis = np.zeros((seg_mask.shape[0], seg_mask.shape[1]))
+  for r in range(vis.shape[0]):
+    for c in range(vis.shape[1]):
+      vis[r][c] = seg_mask[r][c][0]
+    
+  binary = vis > 0
+  plt.imshow(binary, cmap='gray')
+  plt.gca().set_axis_off()
+  plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+  plt.margins(0,0)
+  plt.gca().xaxis.set_major_locator(plt.NullLocator())
+  plt.gca().yaxis.set_major_locator(plt.NullLocator())
+  plt.savefig('/Users/vignavramesh/Downloads/img.png', bbox_inches = 'tight', pad_inches = 0)
 
-bar = progressbar.ProgressBar(maxval=len(series1DCM)/5 + 1, \
-    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+seriesDCMPaths = glob.glob(str(abs_path) + "/*.dcm")
 
-shape = dicom.read_file(seriesDCM[0]).pixel_array.shape[::-1]
+shape = dicom.read_file(seriesDCMPaths[0]).pixel_array.shape[::-1]
 
-for i in range(len(seriesDCM)):
-  seriesDCM[i] = dicom.read_file(seriesDCM[i])  
+seriesDCM = seriesDCMPaths
+for i in range(len(seriesDCMPaths)):
+  seriesDCM[i] = dicom.read_file(seriesDCMPaths[i])  
 seriesDCM = bubble_sort(seriesDCM)
 
 if not os.path.isdir('output'):
   os.mkdir('output')
 
-seg_mask = np.zeros((shape[0], shape[1], len(seriesDCM))
+seg_mask = np.zeros((shape[0], shape[1], len(seriesDCM)))
 
-bar.start()
-for i in range(len(seriesDCM)):
-  # TODO: Create seg_mask array
-  if i % 5 == 0:
-    bar.update(i/5 + 1)
-bar.finish()
+with open(glob.glob('./files/*')[0]) as f:
+  data = json.load(f)
+data = data["ImageAnnotationCollection"]["imageAnnotations"]["ImageAnnotation"][0]["markupEntityCollection"]["MarkupEntity"]
+
+def find_z(path):
+  for i in range(len(seriesDCMPaths)):
+    if path in seriesDCMPaths[i]:
+      return i
+
+for img in data:
+  coords = img["twoDimensionSpatialCoordinateCollection"]["TwoDimensionSpatialCoordinate"]
+  path = img["imageReferenceUid"]["root"]
+  z = find_z(path)
+  tupVerts = []
+  for coord in coords:
+    x_val = round(coord["x"]["value"])
+    y_val = round(coord["y"]["value"])
+    tupVerts.append((x_val, y_val))
+  
+  x, y = np.meshgrid(np.arange(seg_mask.shape[0]), np.arange(seg_mask.shape[1])) # make a canvas with coordinates
+  x, y = x.flatten(), y.flatten()
+  points = np.vstack((x,y)).T 
+
+  p = Path(tupVerts) # make a polygon
+  grid = p.contains_points(points)
+  mask = grid.reshape(seg_mask.shape[0],seg_mask.shape[1])
+
+  for r in range(seg_mask.shape[0]):
+    for c in range(seg_mask.shape[1]):
+      seg_mask[r][c][z] = mask[r][c]
 
 import matlab.engine
 eng = matlab.engine.start_matlab()
